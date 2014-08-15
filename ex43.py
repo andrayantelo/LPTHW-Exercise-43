afb0172bac3103af6d45b0ce904cd695b07b0431
Andrea Anaya <andrayantelo@gmail.com>
2014-08-14 21:46:54 -0500

    modified all large text blocks

--- ex43.py
+++ ex43.py
@@ -15,7 +15,6 @@ class Scene(object):
         
         
     def slow_print(self, text):
-        #lines = textwrap.dedent(text)
         lines = text.split('\n')
         for line in lines:
             print line
@@ -50,6 +49,10 @@ class CentralCorridor(Scene):
             I'm the part of a bird that's not in the sky,
             I can swim in the ocean and remain dry.
             What am I\'?""")
+        self.riddle_text = textwrap.dedent(
+        """ \n\n\n
+        The Gothon steps aside and lets you pass.
+        You have obtained item Laser Gun.""")
         self.description = 'Central Corridor'
         self.items.append('Laser Gun')
     
@@ -61,11 +64,8 @@ class CentralCorridor(Scene):
         answer = str(raw_input('> '))
         answer = answer.lower()
         if answer == 'a bird\'s shadow':
-            riddle_text = """ 
-            The Gothon steps aside and lets you pass.
-            You have obtained item Laser Gun."""
-            riddle_text = textwrap.dedent(riddle_text)
-            print riddle_text
+            
+            print self.riddle_text
             #self.items.append('Laser Gun')
             #LaserWeaponArmory()
         else:
@@ -77,12 +77,20 @@ test.enter()
 
 
 class LaserWeaponArmory(Scene):
+    
+    def __init__(self):
+        super(LaserWeaponArmory, self).__init__()
+        self.laser_entry = textwrap.dedent(
+        """\n\n\n
+        You know there is a bomb in the vault in the room. Only the
+        captain knows the code for the keypad to the vault. The captain
+        is likely dead so it is up to you to figure it out!""")
+        self.description = 'Laser Weapon Armory'
 
     def enter(self):
-        print "You have entered the Laser Weapon Armory."
-        print "You know there is a bomb in the vault in the room."
-        print "Only the captain knows the code for the keypad to the vault."
-        print "The captain is likely dead so it is up to you to figure it out."
+        super(LaserWeaponArmory, self).enter()
+        
+        self.slow_print(self.laser_entry)
         
         code = str(raw_input('Input 4 numbers please.\n\n> '))
         
@@ -103,40 +111,54 @@ class LaserWeaponArmory(Scene):
             print "Wrong code."
             # I want a person to get three tries. then it's game over.
             
-#test2 = LaserWeaponArmory()
-#test2.enter()
+test2 = LaserWeaponArmory()
+test2.enter()
         
 class TheBridge(Scene):
+    
+    def __init__(self):
+        super(TheBridge, self).__init__()
+        self.bridge_entry = textwrap.dedent(
+        """ \n\n\n
+        All you need to do is cross this bridge and get to the esape pods!
+        Oh no! There is a Gothon standing in your way. The Gothon snarls.
+        With a glean in it's eye he reaches for his club. 
+        'ROAAAAAAAAAAAAAAAAAAAAAAR!' The Gothon charges.""")
+        self.gun_text = textwrap.dedent(
+        """\n\n\n
+        You quickly remember you picked up a laser gun earlier. You aim 
+        at the Gothon and start shooting like crazy.
+        PEW PEW PEW!
+        The Gothon screams in agony as he clutches his laser wounds. The
+        Gothon collapses and bleeds a gooey green substance. It is over.
+        The Gothon is dead. You feel bad for him for a second and then 
+        hear the ship's alarms. You hurriedly run towards the escape pod
+        room, stepping over the suffering Gothon.""")
+        self.grenade_text = textwrap.dedent(
+        """\n\n\n
+        You remember you picked up a grenade in the previous room. You 
+        quickly get it out of your pack and pull the pin. You throw it in 
+        direction of the Gothon and dive behind a pillar. 
+        BOOOOOOOOOOOOOOMBABABABABBABABABABABBAABBOOOOOOMBOOOOOOOM!
+        It's a miracle you're sitll alive and not deaf. You peek out from 
+        behind the pillar. Gothon guts. Gothon guts everywhere.
+        The good news is the coast is clear. You make a break for the escape
+        pod, being careful not to slip on any Gothon guts.""")
+        
+        self.description = 'The Bridge'
 
     def enter(self):
-        print "\n\n\nAll you need to do is cross this bridge and get to the escape pods!"
-        print "Oh no! There is a Gothon standing in your way."
-        print "The Gothon snarls. With a gleam in his eye he reaches for his club."
-        print "ROAAAAAAAAAAAAAAAAAR! The Gothon charges."
+        super(TheBridge, self).enter()
+        self.slow_print(self.bridge_entry)
         
         while True: 
             use_gun = raw_input('Type a command\n\n> ')
          
             if "gun" in use_gun:
-                print "You quickly remember you picked up a laser gun."
-                print "You aim at the Gothon and start shooting like crazy."
-                print "PEW PEW PEW!"
-                print "The Gothon screams in agony as he clutches his laser wounds."
-                print "The Gothon collapses and bleeds a gooey green substance."
-                print "It is over. The Gothon is dead."
-                print "You feel bad for him for a second and then hear the ship's alarms."
-                print "Quickly you run towards the escape pod room."
+                self.slow_print(self.gun_text)
                 #EscapePod
             elif "grenade" in use_gun:
-                print "You remember you picked up a grenade in the previous room."
-                print "You quickly get it out of your pack and pull the pin"
-                print "You throw it in the Gothon's direction and dive behind a pillar"
-                print "BOOOOOOOOOOOOMBABABABABABABABAOOOOOOMOBOOOOOM"
-                print "It's a miracle you're still alive and not deaf."
-                print "You peek out from behind the pillar."
-                print "Gothon guts. Gothon guts everywhere."
-                print "The good news is the coast is clear."
-                print "You make a run for the escape pod."
+                self.slow_print(self.grenade_text)
                 #EscapePod
             elif "bomb" in use_gun:
                 print "No, no. That's for blowing up the ship as you escape."
@@ -151,11 +173,30 @@ class TheBridge(Scene):
 #bridge_test.enter()
          
 class EscapePod(Scene):
+    
+    def __init__(self):
+        super(EscapePod, self).__init__()
+        self.description = 'Escape Pod Room'
+        self.escape_entry = textwrap.dedent(
+        """ \n\n\n
+        You burst into the escape pod room. There are five escape pods 
+        available. You have the key for one of the escape pods and very 
+        little time left.""")
+        self.pod_text = textwrap.dedent(
+        """\n\n\n
+        The key turns and the escape pod opens! You jump inside and hit 
+        the green start button. Just before the escape pod closes you
+        hurl the bomb into the space ship. As the escape pod begins to 
+        leave the ship you see Gothons running up. One of them picks up 
+        the bomb and looks at it curiously. The escape pod zooms away and 
+        the ship explodes. It's over. You made it. Congratulations. You
+        take a moment to register what just happened and sink into your 
+        seat. 'Everyone I know is dead.' You begin to cry uncontrollably
+        as the pod makes it's way down to your home planet. """)
 
     def enter(self):
-        print "You burst into the escape pod room."
-        print "There are 5 escape pods available." 
-        print "You have a key for one of the escape pods and very little time left."
+        super(EscapePod, self).enter()
+        self.slow_print(self.escape_entry)
         escape = str(raw_input('Which escape pod do you pick (1-5)?\n\n> '))
         
         if escape == '1':
@@ -168,22 +209,13 @@ class EscapePod(Scene):
             pass
             #death
         elif escape == '4':
-            print "\n\n\nThe key turns and the escape pod opens!"
-            print "You jump inside and hit the green start button."
-            print "Just before the escape pod closes you hurl the bomb into the space ship."
-            print "As the escape pod begins to leave the ship you see Gothons running up."
-            print "One of them picks up the bomb and looks at it curiously."
-            print "The escape pod zooms away and the ship explodes."
-            print "It's over. You made it. Congratulations."
-            print "You take a moment to register what just happened and sink into your seat."
-            print "'Everyone I know is dead.'"
-            print "You begin to cry uncontrollably as the pod makes it's way down to your home planet."
+            self.slow_print(self.pod_text)
         elif escape == '5':
             pass
             #death
 
-#escape_test = EscapePod()
-#escape_test.enter()
+escape_test = EscapePod()
+escape_test.enter()
 
         
 class Map(object):
